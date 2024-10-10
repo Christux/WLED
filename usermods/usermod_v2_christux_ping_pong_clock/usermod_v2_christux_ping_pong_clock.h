8f4e7f47a0b4f470e332ebacc2571352d89f9795
Christux <christux57@free.fr>
2024-10-10 18:39:39 +0200

    code cleanup

--- usermods/usermod_v2_christux_ping_pong_clock/usermod_v2_christux_ping_pong_clock.h
+++ usermods/usermod_v2_christux_ping_pong_clock/usermod_v2_christux_ping_pong_clock.h
@@ -2,25 +2,18 @@
 
 #include "wled.h"
 
-/*
- * Usermods allow you to add own functionality to WLED more easily
- * See: https://github.com/Aircoookie/WLED/wiki/Add-own-functionality
+/**
+ * Clock panel design :
  * 
- * This is an example for a v2 usermod.
- * v2 usermods are class inheritance based and can (but don't have to) implement more functions, each of them is shown in this example.
- * Multiple v2 usermods can be added to one compilation easily.
- * 
- * Creating a usermod:
- * This file serves as an example. If you want to create a usermod, it is recommended to use usermod_v2_empty.h from the usermods folder as a template.
- * Please remember to rename the class and file to a descriptive name.
- * You may also use multiple .h and .cpp files.
- * 
- * Using a usermod:
- * 1. Copy the usermod into the sketch folder (same folder as wled00.ino)
- * 2. Register the usermod by adding #include "usermod_filename.h" in the top and registerUsermod(new MyUsermodClass()) in the bottom of usermods_list.cpp
+ *           o o     o o         o o     o o
+ *          o o o   o o o   o   o o o   o o o
+ *           o o     o o         o o     o o
+ *          o o o   o o o   o   o o o   o o o
+ *           o o     o o         o o     o o
+ *          \___/   \___/  \_/  \___/   \___/
+ * Digit# :   1       2           3       4
  */
 
-//class name. Use something descriptive and leave the ": public Usermod" part :)
 class ChristuxPingPongClockUsermod : public Usermod {
 
   private:
@@ -68,16 +61,10 @@ class ChristuxPingPongClockUsermod : public Usermod {
 
     static const uint32_t blank = RGBW32(0,0,0,0);
 
-    // any private methods should go here (non-inline method should be defined out of class)
-    void publishMqtt(const char* state, bool retain = false); // example for publishing MQTT message
-
     int getLedPosition(int i, int j);
-
     void displayDigit(int position, int n);
 
   public:
-
-    // non WLED related methods, may be used for data exchange between usermods (non-inline methods should be defined out of class)
 
     /**
      * Enable/Disable the usermod
@@ -88,8 +75,6 @@ class ChristuxPingPongClockUsermod : public Usermod {
      * Get usermod enabled/disabled state
      */
     inline bool isEnabled() { return enabled; }
-
-    // methods called by WLED (can be inlined as they are called only once but if you call them explicitly define them out of class)
 
     /*
      * setup() is called once at boot. WiFi is not yet connected at this point.
@@ -102,25 +87,8 @@ class ChristuxPingPongClockUsermod : public Usermod {
       initDone = true;
     }
 
-
-    /*
-     * connected() is called every time the WiFi is (re)connected
-     * Use it to initialize network interfaces
-     */
-    void connected() {
-      //Serial.println("Connected to WiFi!");
-    }
-
-
     /*
      * loop() is called continuously. Here you can check for events, read sensors, etc.
-     * 
-     * Tips:
-     * 1. You can use "if (WLED_CONNECTED)" to check for a successful network connection.
-     *    Additionally, "if (WLED_MQTT_CONNECTED)" is available to check for a connection to an MQTT broker.
-     * 
-     * 2. Try to avoid using the delay() function. NEVER use delays longer than 10 milliseconds.
-     *    Instead, use a timer check as shown here.
      */
     void loop() {
       // if usermod is disabled or called during strip updating just exit
@@ -146,70 +114,10 @@ class ChristuxPingPongClockUsermod : public Usermod {
       if (user.isNull()) user = root.createNestedObject("u");
     }
 
-
-    /*
-     * addToJsonState() can be used to add custom entries to the /json/state part of the JSON API (state object).
-     * Values in the state object may be modified by connected clients
-     */
-    void addToJsonState(JsonObject& root)
-    {
-      if (!initDone || !enabled) return;  // prevent crash on boot applyPreset()
-
-      JsonObject usermod = root[FPSTR(_name)];
-      if (usermod.isNull()) usermod = root.createNestedObject(FPSTR(_name));
-    }
-
-
-    /*
-     * readFromJsonState() can be used to receive data clients send to the /json/state part of the JSON API (state object).
-     * Values in the state object may be modified by connected clients
-     */
-    void readFromJsonState(JsonObject& root)
-    {
-      if (!initDone) return;  // prevent crash on boot applyPreset()
-
-      JsonObject usermod = root[FPSTR(_name)];
-      if (!usermod.isNull()) {
-        // expect JSON usermod data in usermod name object: {"ExampleUsermod:{"user0":10}"}
-        userVar0 = usermod["user0"] | userVar0; //if "user0" key exists in JSON, update, else keep old value
-      }
-    }
-
-
     /*
      * addToConfig() can be used to add custom persistent settings to the cfg.json file in the "um" (usermod) object.
      * It will be called by WLED when settings are actually saved (for example, LED settings are saved)
      * If you want to force saving the current state, use serializeConfig() in your loop().
-     * 
-     * CAUTION: serializeConfig() will initiate a filesystem write operation.
-     * It might cause the LEDs to stutter and will cause flash wear if called too often.
-     * Use it sparingly and always in the loop, never in network callbacks!
-     * 
-     * addToConfig() will make your settings editable through the Usermod Settings page automatically.
-     *
-     * Usermod Settings Overview:
-     * - Numeric values are treated as floats in the browser.
-     *   - If the numeric value entered into the browser contains a decimal point, it will be parsed as a C float
-     *     before being returned to the Usermod.  The float data type has only 6-7 decimal digits of precision, and
-     *     doubles are not supported, numbers will be rounded to the nearest float value when being parsed.
-     *     The range accepted by the input field is +/- 1.175494351e-38 to +/- 3.402823466e+38.
-     *   - If the numeric value entered into the browser doesn't contain a decimal point, it will be parsed as a
-     *     C int32_t (range: -2147483648 to 2147483647) before being returned to the usermod.
-     *     Overflows or underflows are truncated to the max/min value for an int32_t, and again truncated to the type
-     *     used in the Usermod when reading the value from ArduinoJson.
-     * - Pin values can be treated differently from an integer value by using the key name "pin"
-     *   - "pin" can contain a single or array of integer values
-     *   - On the Usermod Settings page there is simple checking for pin conflicts and warnings for special pins
-     *     - Red color indicates a conflict.  Yellow color indicates a pin with a warning (e.g. an input-only pin)
-     *   - Tip: use int8_t to store the pin value in the Usermod, so a -1 value (pin not set) can be used
-     *
-     * See usermod_v2_auto_save.h for an example that saves Flash space by reusing ArduinoJson key name strings
-     * 
-     * If you need a dedicated settings page with custom layout for your Usermod, that takes a lot more work.  
-     * You will have to add the setting to the HTML, xml.cpp and set.cpp manually.
-     * See the WLED Soundreactive fork (code and wiki) for reference.  https://github.com/atuline/WLED
-     * 
-     * I highly recommend checking out the basics of ArduinoJson serialization and deserialization in order to use custom settings!
      */
     void addToConfig(JsonObject& root)
     {
@@ -217,47 +125,17 @@ class ChristuxPingPongClockUsermod : public Usermod {
       top[FPSTR(_enabled)] = enabled;
     }
 
-
     /*
      * readFromConfig() can be used to read back the custom settings you added with addToConfig().
      * This is called by WLED when settings are loaded (currently this only happens immediately after boot, or after saving on the Usermod Settings page)
-     * 
-     * readFromConfig() is called BEFORE setup(). This means you can use your persistent values in setup() (e.g. pin assignments, buffer sizes),
-     * but also that if you want to write persistent values to a dynamic buffer, you'd need to allocate it here instead of in setup.
-     * If you don't know what that is, don't fret. It most likely doesn't affect your use case :)
-     * 
-     * Return true in case the config values returned from Usermod Settings were complete, or false if you'd like WLED to save your defaults to disk (so any missing values are editable in Usermod Settings)
-     * 
-     * getJsonValue() returns false if the value is missing, or copies the value into the variable provided and returns true if the value is present
-     * The configComplete variable is true only if the "exampleUsermod" object and all values are present.  If any values are missing, WLED will know to call addToConfig() to save them
-     * 
-     * This function is guaranteed to be called on boot, but could also be called every time settings are updated
      */
     bool readFromConfig(JsonObject& root)
     {
-      // default settings values could be set here (or below using the 3-argument getJsonValue()) instead of in the class definition or constructor
-      // setting them inside readFromConfig() is slightly more robust, handling the rare but plausible use case of single value being missing after boot (e.g. if the cfg.json was manually edited and a value was removed)
-
       JsonObject top = root[FPSTR(_name)];
-
       bool configComplete = !top.isNull();
-
       configComplete &= getJsonValue(top["enabled"], enabled);
-
       return configComplete;
     }
-
-
-    /*
-     * appendConfigData() is called when user enters usermod settings page
-     * it may add additional metadata for certain entry fields (adding drop down is possible)
-     * be careful not to add too much as oappend() buffer is limited to 3k
-     */
-    void appendConfigData()
-    {
-      
-    }
-
 
     /*
      * handleOverlayDraw() is called just before every show() (LED strip update frame) after effects have set the colors.
@@ -281,35 +159,6 @@ class ChristuxPingPongClockUsermod : public Usermod {
       }
     }
 
-
-#ifndef WLED_DISABLE_MQTT
-    /**
-     * handling of MQTT message
-     * topic only contains stripped topic (part after /wled/MAC)
-     */
-    bool onMqttMessage(char* topic, char* payload) {
-      return false;
-    }
-
-    /**
-     * onMqttConnect() is called when MQTT connection is established
-     */
-    void onMqttConnect(bool sessionPresent) {
-      // do any MQTT related initialisation here
-      //publishMqtt("I am alive!");
-    }
-#endif
-
-
-    /**
-     * onStateChanged() is used to detect WLED state change
-     * @mode parameter is CALL_MODE_... parameter used for notifications
-     */
-    void onStateChange(uint8_t mode) {
-      // do something if WLED state changed (color, brightness, effect, preset, etc)
-    }
-
-
     /*
      * getId() allows you to optionally give your V2 usermod an unique ID (please define it in const.h!).
      * This could be used in the future for the system to determine whether your usermod is installed.
@@ -318,31 +167,13 @@ class ChristuxPingPongClockUsermod : public Usermod {
     {
       return USERMOD_ID_CHRISTUX_PING_PONG_CLOCK;
     }
-
-   //More methods can be added in the future, this example will then be extended.
-   //Your usermod will remain compatible as it does not need to implement all methods from the Usermod base class!
 };
-
 
 // add more strings here to reduce flash memory usage
 const char ChristuxPingPongClockUsermod::_name[]    PROGMEM = "ChristuxPingPongClock";
 const char ChristuxPingPongClockUsermod::_enabled[] PROGMEM = "enabled";
 
-
 // implementation of non-inline member methods
-
-void ChristuxPingPongClockUsermod::publishMqtt(const char* state, bool retain)
-{
-#ifndef WLED_DISABLE_MQTT
-  //Check if MQTT Connected, otherwise it will crash the 8266
-  if (WLED_MQTT_CONNECTED) {
-    char subuf[64];
-    strcpy(subuf, mqttDeviceTopic);
-    strcat_P(subuf, PSTR("/example"));
-    mqtt->publish(subuf, 0, retain, state);
-  }
-#endif
-}
 
 int ChristuxPingPongClockUsermod::getLedPosition(int i, int j) 
 {
@@ -352,7 +183,7 @@ int ChristuxPingPongClockUsermod::getLedPosition(int i, int j)
 void ChristuxPingPongClockUsermod::displayDigit(int position, int digit)
 {
   for(int i = 0, pos = 0; i < 5; i++) {
-    for (int j = 0, k = i%2; j < 2 + k; j++) {
+    for (int j = 0, k = i & 0x01; j < 2 + k; j++) {
       if(!_numbers[digit][pos]) {
         strip.setPixelColor(i * 25 + 2 * j + 1 - k + position, blank);
       }
